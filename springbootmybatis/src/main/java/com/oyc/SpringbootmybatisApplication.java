eedec0bdb38d4854a2946a787253fd29e4e2cb97
francisoyc <619491800@qq.com>
2018-10-30 22:15:38 +0800

    Update SpringbootmybatisApplication.java

--- springbootmybatis/src/main/java/com/oyc/SpringbootmybatisApplication.java
+++ springbootmybatis/src/main/java/com/oyc/SpringbootmybatisApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootmybatisApplication {
 
 	public static void main(String[] args) {
+		System.out.println("123456");
 		SpringApplication.run(SpringbootmybatisApplication.class, args);
 	}
 }
