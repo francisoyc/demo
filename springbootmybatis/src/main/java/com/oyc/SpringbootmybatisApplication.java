cf049ba19e82e88db78ee8eabbde0dd391c727d8
oyc <619491800@qq.com>
2018-10-31 07:17:28 +0800

    ceshi

--- springbootmybatis/src/main/java/com/oyc/SpringbootmybatisApplication.java
+++ springbootmybatis/src/main/java/com/oyc/SpringbootmybatisApplication.java
@@ -11,5 +11,6 @@ public class SpringbootmybatisApplication {
 	public static void main(String[] args) {
 		System.out.println("asdknslng");
 		SpringApplication.run(SpringbootmybatisApplication.class, args);
+		System.out.println("fag");
 	}
 }
